e742fc7cf7fdebeb2ff73455638a9527931d61ec
Gustaf Broström <gustaf-96@hotmail.com>
2020-05-27 20:34:54 +0200

    m

--- backend/stock_data.py
+++ backend/stock_data.py
@@ -1,7 +1,8 @@
 import yfinance as yf
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
-def plot_stock(symbols, start, end=None):
-	data = yf.download(tickers=symbols, start=start, end=end)
-	data.plot(kind='line',x=data["Date"], y= data["High"])
+def plot_stock(symbols, start, end=None, interval=None):
+	data = yf.download(tickers=symbols, start=start, end=end, interval=interval)
+	data.plot(kind='line', y="High")
+	plt.show()
--- yahoo.py
+++ yahoo.py
@@ -1,1 +1,3 @@
-from backend import stock_data
+from backend import stock_data as sd
+
+sd.plot_stock("AAPL", start="2020-05-21", interval="1m")
