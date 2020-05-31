7254b5e3e75cd747384115f1c7211812bd85a32d
Gustaf Broström <gustaf-96@hotmail.com>
2020-05-31 11:19:54 +0200

    m

--- trading_algorithms/gustafs_moneymaker/main.py
+++ trading_algorithms/gustafs_moneymaker/main.py
@@ -1,1 +1,10 @@
+from datetime import datetime
 
+
+class Bot:
+
+	def __init__(self):
+		self.name = "boiiii"
+
+	def algorithm(self, data):
+		return {"actions": [(datetime.fromtimestamp(1545730073),"buy",1000), (datetime.fromtimestamp(1545770073),"sell",1000)]}
