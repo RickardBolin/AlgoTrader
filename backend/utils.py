0344db85cf900dcc4b3f0a8119df534235130d38
Rickard Bolin <ribo1901@student.miun.se>
2020-06-05 20:35:25 +0200

    m

--- backend/utils.py
+++ backend/utils.py
@@ -14,8 +14,13 @@ def convert_timestamp_to_datetime(timestamp):
         return datetime.strptime(str(timestamp), '%Y-%m-%d %H:%M:%S')
     elif timestamp_length == 25:
         dt = datetime.strptime(str(timestamp)[:19], '%Y-%m-%d %H:%M:%S')
-        shift = datetime.strptime(str(timestamp)[19:], '-%H:%M')
-        dt = datetime.replace(dt, hour=dt.hour + shift.hour, minute=dt.minute + shift.minute)
+        sign = str(timestamp)[19]
+        hour_shift = datetime.strptime(str(timestamp)[20:], '%H:%M').hour
+        minute_shift = datetime.strptime(str(timestamp)[20:], '%H:%M').minute
+        if sign == "+":
+            hour_shift *= -1
+            minute_shift *= -1
+        dt = datetime.replace(dt, hour=(dt.hour + hour_shift) % 24, minute=(dt.minute + minute_shift)%60)
         return dt
     else:
         print("Wrong format/lengths! The length of the timestamp is " + str(timestamp_length) + ". Only 10, 19 and 25 currently allowed.")
