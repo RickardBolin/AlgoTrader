a242917102782926321fa3e7b5b2e01c6b22d918
erostedt <erostedt@github.com>
2020-06-11 14:42:06 +0200

    test sde

--- backend/SDE.py
+++ backend/SDE.py
@@ -3,6 +3,9 @@ import pandas as pd
 
 """
 File that shall be filled with functions related to Stochastic Differential Equations.
+
+Could be relevant:
+https://github.com/mattja/sdeint/tree/master/sdeint
 """
 
 
@@ -13,12 +16,12 @@ def brownian_motion(steps):
     :return:
     """
     dt = 1./steps
-    b = np.random.normal(0, 1, steps) * np.sqrt(dt)
+    b = np.random.normal(0, np.sqrt(dt), steps)
     W = np.cumsum(b)
     return W
 
 
-def gbm(start_price, avg_return, vol, steps):
+def gbm(start_price, avg_return, vol, steps, bm=None):
 
     """
     GBM = geometric brownian motion, is the name of the standard stochastic differential equation for modeling
@@ -36,14 +39,49 @@ def gbm(start_price, avg_return, vol, steps):
     """
 
     t = np.arange(steps)
-    bm = brownian_motion(steps)
-    returns = np.zeros(steps)
+    if not bm:
+        bm = brownian_motion(steps)
+    stock_price = np.zeros(steps)
+    mu, sigma = avg_return * steps, vol * np.sqrt(steps)
+
     for step in range(steps):
-        drift = (avg_return - 0.5 * (vol ** 2)) * t[step]
-        diffusion = vol * bm[step]
-        returns[step] = start_price * np.exp(drift + diffusion)
+
+        drift = (mu - 0.5 * (sigma ** 2)) * t[step]
+        diffusion = sigma * bm[step]
+        stock_price[step] = start_price * np.exp(drift + diffusion)
+
+    return stock_price
+
+
+def ornstein_uhlenbeck(start_price, avg_return, vol, steps, theta=0):
+    """
+    Solver to the SDE:
+
+    dS_t = theta * (mean - S_t) * dt + std * dB_t
+
+    method: Euler-Maruyama -> S_t = S_{t-1} + a(S_{t-1}) dt + b(S_{t-1}) dB_{t-1}
+
+    :param start_price: Initial stock price.
+    :param avg_return: Expected average return
+    :param vol: Expected (average) volatility
+    :param steps: Length of process.
+    :param theta: Optional scaling term for the drift (optional).
+    :return: Estimated future returns based on the Ornstein-Uhlenbeck SDE.
+    """
+
+    # t = t(start, end, steps), dt = t[1:] - t[:-1]. But we always have unit step so dt = 1.
+    dt = 1
+    bm = brownian_motion(steps)
+    dB = bm[1:] - bm[:-1]
+    returns = np.zeros(steps)
+    returns[0] = start_price
+    for step in range(1, steps):
+        returns[step] = returns[step-1] + theta * (avg_return - returns[step-1]) * dt + vol * dB[step-1]
     return returns
 
 
-def euler_maruyama():
+def runge_kutta():
+    """
+    Might be added later.
+    """
     pass
